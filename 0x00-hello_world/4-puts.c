e44ec2ddff35b1421b40a9d43dfac8345085fda1
Briankogi <jbriankogi254@gmail.com>
2023-02-16 12:18:24 -0800

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* this is a program to output a sentense using puts */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"/n);
+	Return (0)
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* This is a program that prints a sentence using printf */
+int main(void)
+{
+	printf("with proper grammer,but the outcome is a piece of art,"/n);
+	Return (0)
+}
