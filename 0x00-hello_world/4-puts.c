73e396654e480ff9b342d605bab785f8fa9e403b
Briankogi <jbriankogi254@gmail.com>
2023-02-16 15:32:14 -0800

    v

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - This is a program to output a sentense using puts
+ * 
  * Return: 0 (success)
  **/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\ n");
-	Return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return 0;
 }
