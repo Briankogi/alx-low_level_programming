b137e89f40bae5aba7828690f31412aa504c49a2
Briankogi <briankogi254@gmail.com>
2023-02-20 21:48:19 +0000

    c

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - prints "Programming is like building a multilingual puzzle
+*main(void) - prints "Programming is like building a multilingual puzzle
 *
-*Return Always 0 (Success)
+*return Always 0 (Success)
 */
 void main(void)
 {
