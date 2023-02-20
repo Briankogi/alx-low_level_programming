79f572af4e6e73e36a4925243218dab18413391f
Briankogi <briankogi254@gmail.com>
2023-02-20 21:59:40 +0000

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main -  Entrypoint for this program
  * Return: Always 0 (success)
  */
-void main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
