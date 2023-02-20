4a7d470131681618a884319f558249a1c06253bd
Briankogi <briankogi254@gmail.com>
2023-02-20 20:32:40 +0000

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/**
+/*
  *main - Entry point for this program
  *
  *Return Always 0 (Success)
- */
+**/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n this is unexpected");
 	return (0);
 }
