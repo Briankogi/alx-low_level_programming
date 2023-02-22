16c9acab0fc2acddceaa3a3f8da6ef48e1e2dfab
Briankogi <briankogi254@gmail.com>
2023-02-22 09:57:50 +0000

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
-*main - This is a way to print  using message
+* main - This is a way to print  using message
 *
-*Return Always 0 (Success)
+* Return Always 0 (Success)
 */
-void main(void)
+int main(void)
 {
-	char *message = "and that piece of art is useful\"
-Dora Korpar, 2015-10-19\n";
-	write(2, message, 46);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
