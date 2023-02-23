1b4da7fee6f198b1baa7ff6e9a88719209b47221
Briankogi <briankogi254@gmail.com>
2023-02-23 15:04:00 +0000

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,6 +7,7 @@
 int main(void)
 {
 	int i, j, k;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = i + 1; j < 9; j++)
@@ -14,7 +15,7 @@ int main(void)
 			for (k = j + 1; k < 10; k++)
 			{
 				putchar(i + '0');
-				putchar(j + '0);
+				putchar(j + '0');
 				putchar(k + '0');
 				if (i == 7 && j == 8 && k == 9)
 				{
@@ -28,6 +29,6 @@ int main(void)
 			}
 		}
 	}
-	return 0;
+	return (0);
 }
 
