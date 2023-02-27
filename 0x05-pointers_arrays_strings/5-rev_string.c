a7438f26b719d4def1f35cb8c0e14693a70dcaa6
Briankogi <briankogi254@gmail.com>
2023-02-27 19:36:26 +0000

    c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,11 @@ void rev_string(char *s)
 {
 	int i, length;
 
-	for (; s[length] != 0; length++)
+	for (; s[length] != '\0'; length++)
 	{
 
 	}
-	length--;
+	int k[length];
 
 	for (i = length; i >= 0; --i)
 	{
