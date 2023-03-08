10a7987a64fde374ec77ce9430bfedf57206a07a
Briankogi <jbriankogi@gmail.com>
2023-03-08 14:44:44 +0000

    c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,14 +2,14 @@
 /**
  * factorial(int n) - returns factorial of a number
  * @n: start number
- * Return: -1 if n < 0 
+ * Return: -1 if n < 0
  */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	else if (n == 0)
-		return 1;
+		return (1);
 	else
 		return (n * factorial(n - 1));
 }
