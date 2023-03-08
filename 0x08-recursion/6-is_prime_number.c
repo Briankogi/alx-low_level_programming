fc642559c47a3acf310c803256a63870c89af665
Briankogi <jbriankogi@gmail.com>
2023-03-08 16:06:10 +0000

    c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
  */
 int is_prime_number(int n)
 {
-	int a, deci;
+	int a, deci, decim, d, dec;
 
 	if (n < 2)
 		return (0);
@@ -15,6 +15,14 @@ int is_prime_number(int n)
 		return (1)
 	if (n > 2)
 	{
-		if (x = )
+		decim = n % 2;
+		deci = n % 3;
+		d = n % 5;
+		dec = n % 7;
+
+		if (decim != 0 && deci != 0 && dec != 0 && d != 0)
+			return (1);
+		else
+			return (0);
 	}
 }
